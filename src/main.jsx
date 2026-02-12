f7aa65f0f2f7ae7a034ff92b09206ebf2d784406
Zullox <albertojesusdarias@gmail.com>
2026-02-12 19:15:55 +0100

    Fix App import

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
+@"
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// Usar la versión modular en lugar del monolítico DominoR.jsx
-import App from './App.modular.jsx'
+import App from './App.jsx'
 import './App.css'
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+"@ | Set-Content src\main.jsx -Encoding UTF8
